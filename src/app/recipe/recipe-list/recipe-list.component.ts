070bb110da1e8559c62f41ab3581791e7e3a2d60
Manuel Santana <manuelsantanar03@gmail.com>
2022-11-19 05:30:49 -0400

    Creo la estructura para ver y agregar recetas

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styles: [
-  ]
+  styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
 
+export class RecipeListComponent implements OnInit {
+    recipes:Recipe[] = [
+
+    new Recipe("Pizza","Delicious Pizza","https://cdn.com.do/wp-content/uploads/2017/07/12dc1a9d-pizza.jpg")
+
+]
   constructor() { }
 
   ngOnInit(): void {
